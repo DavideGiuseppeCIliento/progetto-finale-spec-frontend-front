d61b77cacdb3652824460b9e55cae2c3ec75943c
DavideGiuseppeCiliento <cilientodg@gmail.com>
2025-09-08 21:09:29 +0200

    Creata la sezione Hero e un primo esempio di header sticky

--- front-end/src/layouts/Header.jsx
+++ front-end/src/layouts/Header.jsx
@@ -2,5 +2,42 @@
 import { NavLink } from "react-router-dom";
 
 export default function Header() {
-  return;
+  return (
+    <nav className="header-site navbar navbar-expand-lg navbar-dark bg-dark px-5">
+      <a className="navbar-brand" href="#">
+        Navbar
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto">
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Games
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Popular
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -1,14 +1,18 @@
 export default function HomePage() {
   return (
     // ## HERO SECTION
-    <div className="container-fluid homepage-background-hero d-flex flex-column align-items-center justify-content-center text-center">
-      <div className="row homepage-text-hero ">
-        <h1 className="hero-title">Vivi Sperienze Indimenticabili</h1>
-        <h5>Non perderti mai i titoli più emozionanti del momento</h5>
-        <div className="col-auto pt-4">
-          <button className="button-hero">Scopri di più</button>
+    <>
+      <div className="container-fluid homepage-background-hero d-flex flex-column align-items-center justify-content-center text-center">
+        <div className="row homepage-text-hero ">
+          <h1 className="hero-title">Vivi Sperienze Indimenticabili</h1>
+          <h5>Non perderti mai i titoli più emozionanti del momento</h5>
+          <div className="col-auto pt-4">
+            <button className="button-hero">Scopri di più</button>
+          </div>
         </div>
       </div>
-    </div>
+
+      <div className="container ">ciao</div>
+    </>
   );
 }
