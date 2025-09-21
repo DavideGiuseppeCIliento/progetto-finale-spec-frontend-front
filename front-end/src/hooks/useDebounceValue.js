a69084a85761a9536d49d42a574ce2935241e8c3
DavideGiuseppeCiliento <cilientodg@gmail.com>
2025-09-21 20:04:25 +0200

    Inserimento commenti.

--- front-end/src/hooks/useDebounceValue.js
+++ front-end/src/hooks/useDebounceValue.js
@@ -3,9 +3,9 @@ import { useEffect, useState } from "react";
 export default function useDebounceValue(value, delay = 400) {
   const [debounced, setDebounced] = useState(value); // debbounce = inputValue
   useEffect(() => {
-    const t = setTimeout(() => setDebounced(value), delay);
+    const t = setTimeout(() => setDebounced(value), delay); // Ogni volta che value (o delay) cambia, avvio un timer.
     return () => clearTimeout(t);
-  }, [value, delay]);
+  }, [value, delay]); // Ogni volta che value cambia resetto il Timer
 
-  return debounced;
+  return debounced; // ritorno l'ultima versione
 }
